aca021b3f6f6d600cb503bd89cb82ed39180aeaf
Sebastian Valencia <asrudqvist@javerianacali.edu.co>
2017-10-30 16:55:12 +0000

    info db act

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express X' });
 });
+
+
+router.get('/info', function(req, res, next) {
+  var info = process.env.MYSQLCONNSTR_localdb;
+  res.render('index', { title: 'Info', info: info});
+});
+
 
 module.exports = router;
