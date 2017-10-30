8a518966ce5b3eafd9e8768accd68e7106b71099
Sebastian Valencia <asrudqvist@javerianacali.edu.co>
2017-10-30 20:08:42 +0000

    db fix

--- db/conmysql.js
+++ db/conmysql.js
@@ -7,16 +7,16 @@ module.exports = function ()
 	//Database=localdb;Data Source=127.0.0.1:54989;User Id=azure;Password=6#vWHD_$
 	var conInfo = process.env.MYSQLCONNSTR_localdb;
 	
-	function getConnObj (str)
+	function getConnObj (s)
 	{
-		var ds = /Data Source=([^;]+)/.exec(str)[1]
+		var ds = /Data Source=([^;]+)/.exec(s)[1].split(":");
 		return  {
 		  host     : ds[0],
 		  port	   : ds[1],
-		  user     : /User Id==([^;]+)/.exec(s)[1],
+		  user     : /User Id=([^;]+)/.exec(s)[1],
 		  password : /Password=([^;]+)/.exec(s)[1],
 		  database : /Database=([^;]+)/.exec(s)[1]
-		}
+		};
 	}
     
 	this.connect = function(){
